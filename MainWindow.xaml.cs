e63f0f3cfc7d56b6e680bc0a4135c9a72e2b733d
Student S.S <student@uac-icc.ru>
2023-10-04 12:57:49 +0400

    mai commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        int a = 1;
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string log = Log.Text;
+            string parol = Parol.Text;
+
+            while (a > 3)
+            {
+                if (log == "qqwq" && parol == "1234567")
+                {
+                    www.Text = "Доступ разрешён";
+                }
+            } 
+        }
+    }
+}
